2d89c0a73a32e98d72f54325af7d4e68eba84059
Nastoiker <damirdambinov@gmail.com>
2023-07-28 12:46:47 +0400

    fix to https

--- src/services/routerApi.ts
+++ src/services/routerApi.ts
@@ -5,7 +5,7 @@ const routerApi = {
 		const destination = [...routeObject.destination].reverse().join(',')
 		const space = [...routeObject.space].reverse().join(',')
 		const response = await fetch(
-			`http://router.project-osrm.org/route/v1/driving/${original};${space};${destination}?steps=true&geometries=geojson&overview=full`
+			`https://router.project-osrm.org/route/v1/driving/${original};${space};${destination}?steps=true&geometries=geojson&overview=full`
 		)
 		if (!response.ok) {
 			throw new Error(`HTTP error! status: ${response.status}`)
